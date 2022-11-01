7c029b5706d49eacf4e0fe685eee981c3ce7c6b5
SadSAndWiCh _ <caiocamargo007@gmail.com>
2022-11-01 19:13:24 -0300

    first step

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,22 @@
-export default function Home() {
+import { GetServerSideProps } from "next"
+
+type Props = {
+  count: number;
+};
+
+export default function Home({ count }: Props) {
   return (
-    <h1>Hello World</h1>
+    <h1>Contagem: { count }</h1>
   )
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const response = await fetch('http://localhost:3333/pools/count');
+  const data = await response.json();
+
+  return {
+    props: {
+      count: data.count,
+    }
+  }
 }
