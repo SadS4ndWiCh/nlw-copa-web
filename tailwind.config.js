fa866f7209c750c62930949899515cd22751fd5e
SadSAndWiCh _ <caiocamargo007@gmail.com>
2022-11-02 21:53:06 -0300

    add color tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Roboto, sans-serif'
+      },
+
+      colors: {
+        gray: {
+          200: '#E1E1E6',
+          300: '#8D8D99',
+          600: '#323238',
+          700: '#202024',
+          900: '#121214',
+        },
+        green: {
+          500: '#129E57',
+        },
+        yellow: {
+          500: '#F7DD43',
+          600: '#CCB737',
+        }
+      }
+    },
   },
   plugins: [],
 }
